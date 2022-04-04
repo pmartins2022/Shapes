57ef0341b632957dc704095dc383b48466605b66
Marta Moutinho <marta.barbosa-t22@upskill.pt>
2022-04-04 15:39:09 +0100

    rectangle commit

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Rectangle {
+public class Rectangle
+{
+ private Point upperLeft;
+ private Point upperRight;
+
+ public Rectangle(Point p1, Point p2)
+ {
+  this.upperLeft = p1;
+  this.upperRight = p2;
+ }
 }
