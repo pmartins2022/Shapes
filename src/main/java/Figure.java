21707273bddff5f57a93b670ec27aa47ddf695f4
Pedro Martins <pedro.martins-t22@upskill.pt>
2022-04-04 15:32:41 +0100

    added Color, Figure classes

---
+++ src/main/java/Figure.java
@@ -0,0 +1,17 @@
+public abstract class Figure
+{
+    private Color color;
+
+    public Color getColor()
+    {
+        return color;
+    }
+
+    public void setColor(Color c)
+    {
+        color = c;
+    }
+
+    public abstract double area();
+    public abstract double perimeter();
+}
